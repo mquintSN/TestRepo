2825c7da058f0345d91f84b756220351b188fc9a
Mary Quint <mary.k.quint@gmail.com>
2018-03-07 22:47:13 -0600

    logic working

--- game.js
+++ game.js
@@ -1,39 +1,59 @@
 var wins = 0;
 var losses = 0;
 var userTotal = 0;
+var targetNumber = 0;
+var pinkCrystal = 0;
+var greenCrystal =0;
+var yellowCrystal = 0;
+var blueCrystal = 0;
 // Declare global variables
 
 function setValues (){
 	userTotal = 0;
-var targetNumber = Math.floor(Math.random() * 120 + 19);
-var pinkCrystal= Math.floor(Math.random() * 12 + 1);
-var greenCrystal= Math.floor(Math.random() * 12 + 1);
-var blueCrystal= Math.floor(Math.random() * 12 + 1);
-var yellowCrystal= Math.floor(Math.random() * 12 + 1);
+    targetNumber = Math.floor(Math.random() * 120 + 19);
+    pinkCrystal= Math.floor(Math.random() * 12 + 1);
+    greenCrystal= Math.floor(Math.random() * 12 + 1);
+    blueCrystal= Math.floor(Math.random() * 12 + 1);
+    yellowCrystal= Math.floor(Math.random() * 12 + 1);
+
+    $("#goalNumber").text(targetNumber);
+    $("#userTotal").text(userTotal);
+    $("#losses").text(losses);
+    $("#wins").text(wins);
+
+    $("button").animate({opacity: '0.3'}, 100);
+    $("button").animate({opacity: '1'}, 300);
+    // $("button").effect("shake");
 }
 
 // Document.ready thing
 
 setValues()
 
-$(".button").on("click", function() {
+$("button").on("click", function() {
 	if ($(this).hasClass("pinkGem")){
-	userTotal = userTotal + pinkCrystal;
-	$("#userTotal").text(userTotal);
+	    userTotal = userTotal + pinkCrystal;
+	    $("#userTotal").text(userTotal);
    	} else if ( $(this).hasClass("greenGem") ){
 		userTotal = userTotal + greenCrystal; 
-    $("#userTotal").text(userTotal);}
-	// } etc.
+    $("#userTotal").text(userTotal);
+    } else if ( $(this).hasClass("yellowGem") ){
+		userTotal = userTotal + yellowCrystal; 
+     $("#userTotal").text(userTotal);
+    } else if ( $(this).hasClass("blueGem") ){
+		userTotal = userTotal + blueCrystal; 
+        $("#userTotal").text(userTotal);
+    }
+    // } etc.
+    if ( userTotal > targetNumber) {
+        losses ++;
+        setValues()
+    } else if (userTotal === targetNumber) {
+        wins ++;
+        setValues()
+    }
+    
 });
 
 
 
-If ( userTotal > targetNumber) {
-	Losses ++;
-	$("#losses").text(losses);
-	setValues()
-} else if (userTotal === targetNumber) {
-	Wins ++;
-	$("#wins").text(wins);
-	setValues()
-}
